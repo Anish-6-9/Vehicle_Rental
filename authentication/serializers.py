f062b660e8461265899612f869d0f95abbf33c89
Anish-6-9 <anishdeuja9@gmail.com>
2024-05-27 22:41:29 +0545

    done authentication API with simple jwt

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -37,3 +37,24 @@ class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['email', 'name']
+
+
+class UserChangePasswordSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        style={'input_type': 'password'}, write_only=True)
+    password2 = serializers.CharField(
+        style={'input_type': 'password'}, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['password', 'password2']
+
+    def validate(self, attrs):
+        password = attrs['password']
+        password2 = attrs['password2']
+        user = self.context['user']
+        if password != password2:
+            raise serializers.ValidationError("Password doesn't matches.")
+        user.set_password(password)
+        user.save()
+        return attrs
--- authentication/views.py
+++ authentication/views.py
@@ -6,7 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import authenticate
 from .models import User
 
-from .serializers import UserRegistrationSerializer, UserLoginSerializer, UserProfileSerializer
+from .serializers import UserRegistrationSerializer, UserLoginSerializer, UserProfileSerializer, UserChangePasswordSerializer
 from .renderers import UserRenderer
 
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -55,6 +55,17 @@ class UserProfileView(APIView):
     renderer_classes = [UserRenderer]
 
     def get(self, request, format=None):
-        user = User.objects.get(name=request.user.name)
-        serializer = UserProfileSerializer(user)
-        return Response(serializer.data)
+        serializer = UserProfileSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class UserChangePasswordView(APIView):
+    permission_classes = [IsAuthenticated]  # Ensure the user is authenticated
+    renderer_classes = [UserRenderer]
+
+    def post(self, request, format=None):
+        serializer = UserChangePasswordSerializer(
+            data=request.data, context={'user': request.user})
+        if serializer.is_valid(raise_exception=True):
+            return Response({'msg': 'Password changed successfully.'}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
